00f111124039c029aa4200ada9d20c74dd00e4e4
André Marques <115092696+acmmarques@users.noreply.github.com>
2024-08-04 09:27:36 +0100

    added jsx & mdx to treesitter

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -12,3 +12,12 @@ vim.opt.clipboard = 'unnamedplus'
 vim.opt.tabstop = 2       -- Number of visual spaces per TAB
 vim.opt.shiftwidth = 2    -- Number of spaces to use for each step of (auto)indent
 vim.opt.expandtab = true  -- Use spaces instead of TABs
+
+
+-- make mdx files being interpreted as jsx files for treesitter
+vim.cmd [[
+  augroup mdx
+    autocmd!
+    autocmd BufRead,BufNewFile *.mdx set filetype=javascriptreact
+  augroup END
+]]
