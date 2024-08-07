f51d7d4d20a7d611c288213e6faec08a386e1b50
André Marques <115092696+acmmarques@users.noreply.github.com>
2024-08-07 16:25:44 +0100

    added ufo.nvim to the plugin list

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -128,8 +128,6 @@ return {
 		  -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	  }
   },
-
-
   {	  "L3MON4D3/LuaSnip",
   -- follow latest release.
   version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
@@ -145,5 +143,7 @@ return {
 	  lazy = false,
 	  priority = 1000,
 	  opts = {},
-  }
+  },
+  {'kevinhwang91/nvim-ufo',
+    dependencies = 'kevinhwang91/promise-async'}
 }
