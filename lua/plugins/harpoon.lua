9ab73a3e270fe66ea56f4a0616cf76832418e8b4
André Marques <115092696+acmmarques@users.noreply.github.com>
2024-09-26 22:33:34 +0100

    Changed harpoon's file navigation.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,10 +8,8 @@ return {
         vim.keymap.set("n", "<leader>a", mark.add_file)
         vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
 
-        vim.keymap.set("n", "<C-u>", function() ui.nav_file(1) end)
-        vim.keymap.set("n", "<C-i>", function() ui.nav_file(2) end)
-        vim.keymap.set("n", "<C-o>", function() ui.nav_file(3) end)
-        vim.keymap.set("n", "<C-p>", function() ui.nav_file(4) end)
+        vim.keymap.set("n", "<C-u>", function() ui.nav_prev() end)
+        vim.keymap.set("n", "<C-i>", function() ui.nav_next() end)
     end
 }
 
