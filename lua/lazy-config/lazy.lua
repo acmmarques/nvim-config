032bbdd48378ae20a06a3f2e1cbff250c01debe6
André Marques <115092696+acmmarques@users.noreply.github.com>
2025-09-11 20:53:47 +0100

    added shortcut to open lazy pkg manager

--- lua/lazy-config/lazy.lua
+++ lua/lazy-config/lazy.lua
@@ -29,7 +29,10 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "catppuccin" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
+
+-- Set keymap for Lazy using vim.keymap.set
+vim.keymap.set('n', '<leader>la', '<cmd>Lazy<CR>', { noremap = true, silent = true })
