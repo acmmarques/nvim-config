adb38892f38afde07bcf4f1a69173e83ca3c6e47
André Marques <115092696+acmmarques@users.noreply.github.com>
2025-09-11 20:38:17 +0100

    added command to open grep with telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,14 @@
 return {
         'nvim-telescope/telescope.nvim',
         config = function()
+		local telescope = require('telescope')
                 local builtin = require('telescope.builtin')
                 vim.keymap.set('n', '<leader> ', builtin.find_files, {})
+                vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
                 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
                 vim.keymap.set('n', '<leader>gr', function()
                         vim.opt.clipboard = 'unnamedplus'
                         builtin.grep_string({ search = vim.fn.input("Grep > ") })
                 end)
-        end
+	end
 }
